f739047c7df83fd8f408dc3f6597cd06a92b15f0
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2023-10-20 22:33:52 -0400

    fix: onClick doesn't open add shipment modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import "./Header.css";
 function Header(props) {
 	const [modalShow, setModalShow] = useState(false);
 	const [loginShow, setLoginShow] = useState(false);
+	const [addShow, setAddShow] = useState(false);
 
 	return (
 		<Navbar bg="light" variant="light" expand="lg" className="sticky-top">
@@ -16,8 +17,8 @@ function Header(props) {
 						<Nav.Link href="/">Home</Nav.Link>
 						<Nav.Link href="#features">Features</Nav.Link>
 						<Nav.Link href="#pricing">Pricing</Nav.Link>
-						{/* todo: setup "add shipment" button to add new shipments to dashboard */}
-						<Nav.Link>Add Shipment</Nav.Link>{" "}
+						{/* todo: fix onClick not opening modal */}
+						<Nav.Link onClick={() => setAddShow(true)}>Add Shipment</Nav.Link>{" "}
 					</Nav>
 					<Nav>
 						<Nav.Link href="#deets">
@@ -115,6 +116,54 @@ function Header(props) {
 				</Modal.Body>
 				<Modal.Footer>
 					<Button onClick={props.onHide}>Login</Button>
+				</Modal.Footer>
+			</Modal>
+
+			{/* Add Shipment Modal */}
+			<Modal
+				{...props}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+				show={loginShow}
+				onHide={() => setLoginShow(false)}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title-vcenter">
+						ShipTrack
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<h4>Add New Shipment</h4>
+					<Form>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+							<Form.Label>Shipment ID</Form.Label>
+							<Form.Control type="text" placeholder="123456789" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+							<Form.Label>Tracking Number</Form.Label>
+							<Form.Control type="text" placeholder="123456789" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+							<Form.Label>Shipment ID</Form.Label>
+							<Form.Control type="date" placeholder="123456789" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+							<Form.Label>Status</Form.Label>
+							<Form.Control type="date" placeholder="123456789" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+							<Form.Label>Status</Form.Label>
+							<Form.Select>
+								<option>On Time</option>
+								<option>Delayed</option>
+								<option>Arrived</option>
+							</Form.Select>
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={props.onHide}>Add Shipment</Button>
 				</Modal.Footer>
 			</Modal>
 		</Navbar>
