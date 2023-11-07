2f6ac5acaef109bddcb1b177e3591e35443dab92
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2023-11-06 21:08:12 -0500

    added emojies to each card title for more fun

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import L from "leaflet";
+import "leaflet-control-geocoder";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+// import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Row, Table } from "react-bootstrap";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import {
@@ -14,7 +17,25 @@ import {
 
 import "./Dashboard.css";
 
-function Dashboard(props) {
+type MarkerType = {
+	position: [number, number];
+	popup: string;
+};
+
+type ShipmentType = {
+	shipmentID: string;
+	trackingNumber: string;
+	originDestination: string;
+	eta: string;
+	status: string;
+	location: [number, number];
+};
+
+type DashboardProps = {
+	shipments: ShipmentType[];
+};
+
+function Dashboard(props: DashboardProps) {
 	// Sample data for line graph
 	const shipmentData = [
 		{ date: "Jan", air: 4000, sea: 2400, road: 2400 },
@@ -27,7 +48,7 @@ function Dashboard(props) {
 			<Row className="mb-4">
 				<Col>
 					<Card>
-						<Card.Header>World Map & Active Shipments</Card.Header>
+						<Card.Header>📍 World Map & Active Shipments</Card.Header>
 						<Card.Body>
 							<MapContainer
 								center={[51.505, -0.09]}
@@ -35,9 +56,23 @@ function Dashboard(props) {
 								style={{ height: "400px", width: "100%" }}
 							>
 								<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-								<Marker position={[51.505, -0.09]}>
-									<Popup>Active Shipment #123</Popup>
-								</Marker>
+								{/* add a marker for every new shipment added to the watchlist table */}
+								{props.shipments.map((shipment, index) => (
+									<Marker key={index} position={shipment.location}>
+										<Popup>
+											<strong>Shipment ID:</strong> {shipment.shipmentID}
+											<br />
+											<strong>Tracking #:</strong> {shipment.trackingNumber}
+											<br />
+											<strong>Origin/Destination:</strong>{" "}
+											{shipment.originDestination}
+											<br />
+											<strong>ETA:</strong> {shipment.eta}
+											<br />
+											<strong>Status:</strong> {shipment.status}
+										</Popup>
+									</Marker>
+								))}
 							</MapContainer>
 						</Card.Body>
 					</Card>
@@ -47,7 +82,7 @@ function Dashboard(props) {
 			<Row className="mb-4">
 				<Col md={6}>
 					<Card>
-						<Card.Header>Watchlist of Shipments</Card.Header>
+						<Card.Header>🔎 Watchlist of Shipments</Card.Header>
 						<Card.Body>
 							<Table striped bordered hover variant="light">
 								<thead>
@@ -78,7 +113,9 @@ function Dashboard(props) {
 				<Col md={6} style={{ height: "400px" }}>
 					{" "}
 					<Card style={{ height: "100%" }}>
-						<Card.Header>Total Shipments by Mode of Transportation</Card.Header>
+						<Card.Header>
+							📈 Total Shipments by Mode of Transportation
+						</Card.Header>
 						<Card.Body style={{ display: "flex", flexDirection: "column" }}>
 							<ResponsiveContainer width="100%" height="100%">
 								<LineChart data={shipmentData}>
@@ -105,7 +142,7 @@ function Dashboard(props) {
 			<Row className="mb-4">
 				<Col>
 					<Card>
-						<Card.Header>Accounts Receivable</Card.Header>
+						<Card.Header>💰 Accounts Receivable</Card.Header>
 						<Card.Body>
 							<Table striped bordered hover variant="light">
 								<thead>
